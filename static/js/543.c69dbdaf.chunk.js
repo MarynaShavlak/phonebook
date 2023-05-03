2213493a796bf78fadd096bebda973ece0e2d051
MarynaShavlak <111526360+MarynaShavlak@users.noreply.github.com>
2023-05-03 22:13:43 +0000

    Deploying to gh-pages from @ MarynaShavlak/phonebook@f3ddc7b86260f301e1a05864eb7f3ead429ec10e 🚀

---
+++ static/js/543.c69dbdaf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkphonebook=self.webpackChunkphonebook||[]).push([[543],{1543:function(e,n,s){s.r(n);s(2791);var h=s(2633),o=s(6805),u=s(8506),c=s(826),r=s(184);n.default=function(){return(0,r.jsx)(u.FE,{children:(0,r.jsxs)(u.QF,{children:[(0,r.jsx)(h.WS,{}),(0,r.jsx)(o.Vh,{path:c.Z6.ROOT+c.Z6.LOGIN})]})})}}}]);
+//# sourceMappingURL=543.c69dbdaf.chunk.js.map
